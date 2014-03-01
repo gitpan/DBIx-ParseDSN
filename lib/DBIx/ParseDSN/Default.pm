c85320ada4ba7726202507cc75a7c7024294ede5
Torbjørn Lindahl <torbjorn@cpan.org>
2014-03-01 13:47:13 +0000

    Import of TORBJORN/DBIx-ParseDSN-v0.9.3 from CPAN.
    gitpan-cpan-distribution: DBIx-ParseDSN
    gitpan-cpan-version:      v0.9.3
    gitpan-cpan-path:         TORBJORN/DBIx-ParseDSN-v0.9.3.tar.gz
    gitpan-cpan-author:       TORBJORN
    gitpan-cpan-maturity:     released

--- lib/DBIx/ParseDSN/Default.pm
+++ lib/DBIx/ParseDSN/Default.pm
@@ -10,22 +10,23 @@ use Carp qw< carp croak confess cluck >;
 use DBI; # will use parse_dsn from here
 use URI;
 
-use version; our $VERSION = qv('0.9.2');
+use version; our $VERSION = qv('0.9.3');
 
-use Moose;
-use MooseX::Aliases;
+use Moo;
+use namespace::clean;
+use MooX::Aliases;
+use MooX::HandlesVia;
 
-has dsn => ( isa => "Str", is => "ro", required => 1 );
+has dsn => ( is => "ro", required => 1 );
 
-has database => ( isa => "Str", is => "rw", alias => [qw/db dbname/] );
-has host     => ( isa => "Str", is => "rw", alias => "server" );
-has port     => ( isa => "Int", is => "rw" );
-has driver   => ( isa => "Str", is => "rw" );
-has scheme   => ( isa => "Str", is => "rw", default => "dbi" );
+has database => ( is => "rw", alias => [qw/db dbname/] );
+has host     => ( is => "rw", alias => "server" );
+has port     => ( is => "rw" );
+has driver   => ( is => "rw" );
+has scheme   => ( is => "rw", default => "dbi" );
 
 has attr => (
-    isa => "HashRef",
-    traits => ["Hash"],
+    handles_via => "Hash",
     is => "ro",
     default => sub {{}},
     handles => {
@@ -262,7 +263,7 @@ L<DBIx::ParseDSN/parse_dsn> which is the intended way to achieve this.
 
 =head1 VERSION
 
-This document describes DBIx::ParseDSN::Default version 0.9.2
+This document describes DBIx::ParseDSN::Default version 0.9.3
 
 =head1 SYNOPSIS
 
@@ -276,7 +277,7 @@ This document describes DBIx::ParseDSN::Default version 0.9.2
     {
       package DBIx::ParseDSN::OddBall;
 
-      use Moose;
+      use Moo;
       extends 'DBIx::ParseDSN::Default';
 
       sub names_for_database{ return qw/bucket/ }
